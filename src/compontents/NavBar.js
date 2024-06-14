383fb643626c3184389e508b3433e4a38f326857
Parsa <mohammadparsahekmatnia@parsas-macbook-pro.home>
2024-06-14 13:55:48 +0200

    started project

---
+++ src/compontents/NavBar.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'
+
+function NavBar() {
+  return (
+    <>
+    <nav className='nav-bar'>
+        <div className='navbar-container'>
+            <Link to='/' className="navbar-logo">LOGO</Link>
+        </div>
+    </nav>
+    </>
+  )
+}
+
+export default NavBar
